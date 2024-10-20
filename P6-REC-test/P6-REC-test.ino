64574b0e9138a844a4d5165c898376784e27806a
name <luisbalcazar305@gmail.com>
2024-10-20 16:03:38 -0600

    practica 6

--- P6-REC-test/P6-REC-test.ino
+++ P6-REC-test/P6-REC-test.ino
@@ -3,14 +3,14 @@ const int MySRX = 5; // Pin RX para Serial2
 const int MySTX = 18; // Pin TX para Serial2
 EspSoftwareSerial::UART myPort;
 
-String lect = "";
+String lect = "";//variable que usamos para recibir los caracteres de la comunicación uart
 
-int lec[2] = {};
-int i = 0;
+int lec[1] = {};//arreglo para guardar los datos recibidos y poder usarlos como enteros
+int i = 0;//bandera de intercambio entre el primer y segundo dato del arreglo lec[]
 
-int d;
+int d;//variable para guardar la distancia ya procesada en centimetros
 
-String tch = "";
+String tch = "";//variable para guardar el estado del toch en string
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,29 +19,29 @@ void setup() {
 }
 
 void loop() {
-  while(myPort.available()){
-    char msg = myPort.read();
-    lect.concat(msg);
-    if(msg == '\n'){
-      lec[i] = lect.toInt();
-      if(i == 1){
+  while(myPort.available()){//mientras se encuentre disponible un dato en la comunicación
+    char msg = myPort.read();//leemos caracter por caracter 
+    lect.concat(msg);//agregamos a la variable lect para ir guardando los caracteres
+    if(msg == '\n'){//en cuanto detectamos fin de cadena, cambiamos al siguiente espacio de arreglo
+      lec[i] = lect.toInt();//antes de cambiar transformamos a entero y guardamos en su posición del arreglo
+      if(i == 1){//hacemos el intercambio de lugar con la bandera
         i = 0;
       }else{
         i = 1;
       }
-      lect = "";
+      lect = "";//reiniciamos nuestra variable para el siguiente dato
     }
   
     //Serial.print(msg);
-    d = lec[0]/59;
-    if(lec[1] == 1){
+    d = lec[0]/59;//trandormamos los datos del ultrasonico en distancia en centimetros
+    if(lec[1] == 1){//tranformamos en string el valor del touch
       tch = "Presionado";
     } else {
       tch = "No Presionado";
     }
-    String msg_dat = "Distancia: " + String(d) + "cm, Touch: " + tch;
-    Serial.println(msg_dat);
-    Serial.println("-----------");
+    String msg_dat = "Distancia: " + String(d) + "cm, Touch: " + tch;//creamos el paquete de la lectura de los datos en un string
+    Serial.println(msg_dat);//imprimimos en monitor serie
+    Serial.println("-----------");//se utiliza solo para separar las lecturas
   }
   
 
--- Pract6/Pract6.ino
+++ Pract6/Pract6.ino
@@ -9,8 +9,8 @@
 #define usE 4
 #define touch 2
 
-int dis;
-bool tch;
+int dis; //variable para guardar la lectura del sensor ultrasonico
+bool tch; //variable para guardar el estado del touch
 
 const int MySRX = 5; // Pin RX para Serial2
 const int MySTX = 18; // Pin TX para Serial2
@@ -23,7 +23,7 @@ void setup() {
   Serial.begin(115200); // Inicializa el puerto serial USB a 115200 baudios
   myPort.begin(38400, SWSERIAL_8N1, MySRX, MySTX, false); // Inicializa Serial2 con los pines especificados
 
-  pinMode(usT, OUTPUT);
+  pinMode(usT, OUTPUT);//configuramos los pines que vamos a usar
   pinMode(usE, INPUT);
   digitalWrite(usT, 0);
 
@@ -32,53 +32,32 @@ void setup() {
 
 void loop() {
 
-  dis = leerUS();
-  tch = digitalRead(touch);
+  dis = leerUS();//llamamos la funcion de leer el ultrasonico y guardamos su respuesta
+  tch = digitalRead(touch); //leemos la entrada del puerto del touch y guardamos
 
   //String msg_dat = "Distancia: " + String(dis) +"cm, Touch: " + String(tch) + "\n";
 
-  if(myPort.available() > 0){
+  if(myPort.available() > 0){//si tenemos conexion mandamos los dos mensajes con los datos
     String msg0 = String(dis) + '\n'; 
-    myPort.write(msg0.c_str());
+    myPort.write(msg0.c_str());//datos del sensor ultrasonico
     String msg1 = String(tch) + '\n';
-    myPort.write(msg1.c_str());
+    myPort.write(msg1.c_str());//datos del touch
   }else{
-    Serial.println("Error de conexión!");
+    Serial.println("Error de conexión!");//se utiliza para debugear, en caso de no tener una correcta conexion imprime error
   }
-  delay(300);
+  delay(300);//delay para no sobre saturar de datos
   
-
-
-  // Si hay datos disponibles en el puerto serial USB, los lee y los envía a Serial2
-  // if (Serial.available() > 0) {
-  //   String inputString = "PC-2: "+ Serial.readString() + "\n";
-  //   Serial2.write(inputString.c_str()); // Convierte el String a const char* y lo envía
-
-  // }
-
-  // Si hay datos disponibles en Serial2, los lee y los envía al puerto serial USB
-//   if (Serial2.available() > 0) {
-//     String inputString = Serial2.readString();
-//     Serial.write(inputString.c_str()); // Convierte el String a const char* y lo envía
-//   }
  }
 
-int leerUS(){
-  long t;
+int leerUS(){//funcion para leer el ultrasonico
+  long t;//declaramos una variable grande para guardar la distancia
 
-  digitalWrite(usT, 1);
-  delayMicroseconds(10);
-  digitalWrite(usT, 0);
+  digitalWrite(usT, 1);//mandamos un pulso a trigger
+  delayMicroseconds(10);//esperamos 10 microsegundos para que se propage bien
+  digitalWrite(usT, 0);//apagamos el trigger
 
-  t = pulseIn(usE, 1);
+  t = pulseIn(usE, 1);// leermos que ocurrio con la onda
 
-  return t;
+  return t;//regresamos el dato
 }
 
-bool leertch(){
-  if(digitalRead(touch)){
-    return true;
-  }else{
-    return false;
-  }
-}
